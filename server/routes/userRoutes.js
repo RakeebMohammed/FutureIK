b6836c69310c77cba6c5e53a58d4def51bd8b51c
RakeebMohammed <rkbmohd@gmail.com>
2023-06-04 07:57:20 +0530

    unit test done

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,16 +3,15 @@ const {
   Signup,
   Login,
   Check,
-Update
+  Update,
 } = require("../controller/userController");
-
 const router = express.Router();
 //route for signup
 router.post("/register", Signup);
 //route for login
 router.post("/login", Login);
 //route to check email and phone number
-router.post('/forgotPassword',Check)
+router.post("/forgotPassword", Check);
 //route to update password
-router.post('/updatePassword',Update)
+router.post("/updatePassword", Update);
 module.exports = router;
